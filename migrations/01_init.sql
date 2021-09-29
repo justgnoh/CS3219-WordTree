84e833f34d22a6e5640215ae24b91c0975950889
Nikhilalalalala <uvnikhi@gmail.com>
2021-09-29 14:44:37 +0800

    Make tables and populate them for challenges

--- migrations/01_init.sql
+++ migrations/01_init.sql
@@ -1,10 +1,25 @@
-create table book(
-    id SERIAL PRIMARY KEY,
-    link VARCHAR(255),
-    cover VARCHAR(255),
-    title VARCHAR(255),
-    subtitle VARCHAR(255),
-    authors VARCHAR(255)[],
-    description VARCHAR(255),
-    price INTEGER
+DROP TABLE Challenges;
+DROP TABLE Interests;
+DROP TABLE Users;
+
+CREATE TABLE Users(
+    id SERIAL PRIMARY KEY
+);
+
+CREATE TABLE Interests(
+    interest VARCHAR(100) PRIMARY KEY
+);
+
+CREATE TABLE Challenges(
+    challenge_id SERIAL PRIMARY KEY,
+    title VARCHAR(200),
+    squirrel_id INTEGER NOT NULL References Users(id),
+    racoon_id INTEGER References Users(id),
+    num_of_total_turns INTEGER NOT NULL CHECK (num_of_total_turns = 4 OR num_of_total_turns = 6),
+    word_limit_per_turn INTEGER NOT NULL CHECK (word_limit_per_turn = 300 OR word_limit_per_turn = 500),
+    genre VARCHAR(100) NOT NULL References Interests(interest),
+    status_of_challenge VARCHAR(100) NOT NULL DEFAULT 'DRAFT',
+    CHECK (squirrel_id != racoon_id),
+    CHECK ((status_of_challenge = 'DRAFT' OR status_of_challenge = 'WAITING_MATCH') OR racoon_id != NULL),
+    CHECK (status_of_challenge != 'COMPLETED' OR title IS NOT NULL)
 );
--- server/challenge/src/index.ts
+++ server/challenge/src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import booksRouter from './routes/books';
+import challengeRouter from './routes/challenge';
 import bodyParser from 'body-parser';
 
 const PORT = process.env.PORT || 5000;
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/challenge/', booksRouter);
+app.use('/challenge/', challengeRouter);
 
 app.listen(PORT, () => {
     console.log(`Started api service on port: ${PORT}`);
