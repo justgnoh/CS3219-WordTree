3011ec4d7088fb607344b08095ab91296003f1ab
Nikhilalalalala <uvnikhi@gmail.com>
2021-10-07 12:11:28 +0800

    Add turns details table

--- migrations/02_insert_data.sql
+++ migrations/02_insert_data.sql
@@ -13,13 +13,19 @@ insert into Interests (interest) values ('crime');
 insert into Interests (interest) values ('fantasy');
 insert into Interests (interest) values ('horror');
 
-insert into Challenges (challenge_id, title, squirrel_id, racoon_id, num_of_total_turns, word_limit_per_turn, genre) values (1, 'Zoozzy', 5, 4, 4, 300, 'crime');
-insert into Challenges (challenge_id, title, squirrel_id, racoon_id, num_of_total_turns, word_limit_per_turn, genre) values (2, 'Oyonder', 2, 5, 4, 500, 'crime');
-insert into Challenges (challenge_id, title, squirrel_id, racoon_id, num_of_total_turns, word_limit_per_turn, genre) values (3, 'Oyondu', 5, 6, 6, 300, 'horror');
-insert into Challenges (challenge_id, squirrel_id, racoon_id, num_of_total_turns, word_limit_per_turn, genre) values (4, 8, 2, 4, 500, 'horror');
-insert into Challenges (challenge_id, squirrel_id, racoon_id, num_of_total_turns, word_limit_per_turn, genre) values (5,  9, 1, 6, 300, 'horror');
-insert into Challenges (challenge_id, title, squirrel_id, racoon_id, num_of_total_turns, word_limit_per_turn, genre) values (6, 'Feedfish', 3, 1, 4, 300, 'crime');
-insert into Challenges (challenge_id, title, squirrel_id, racoon_id, num_of_total_turns, word_limit_per_turn, genre) values (7, 'Nlounge', 2, 6, 4, 300, 'fantasy');
-insert into Challenges (challenge_id, squirrel_id, racoon_id, num_of_total_turns, word_limit_per_turn, genre) values (8, 2, 7, 6, 500, 'crime');
-insert into Challenges (challenge_id, title, squirrel_id, racoon_id, num_of_total_turns, word_limit_per_turn, genre) values (9, 'Jaxbean', 10, 2, 6, 500, 'fantasy');
-insert into Challenges (challenge_id, title, squirrel_id, racoon_id, num_of_total_turns, word_limit_per_turn, genre) values (10, 'Kwimbee', 3, 4, 4, 500, 'fantasy');
+insert into Challenges (challenge_id, title, squirrel_id, num_of_total_turns, word_limit_per_turn, genre) values (1, 'Zoozzy', 5,  4, 300, 'crime');
+insert into Challenges (challenge_id, title, squirrel_id, num_of_total_turns, word_limit_per_turn, genre) values (2, 'Oyonder', 2, 4, 500, 'crime');
+insert into Challenges (challenge_id, title, squirrel_id, num_of_total_turns, word_limit_per_turn, genre) values (3, 'Oyondu', 5, 6, 300, 'horror');
+insert into Challenges (challenge_id, squirrel_id, num_of_total_turns, word_limit_per_turn, genre) values (4, 8, 4, 500, 'horror');
+insert into Challenges (challenge_id, squirrel_id, num_of_total_turns, word_limit_per_turn, genre) values (5, 9, 6, 300, 'horror');
+insert into Challenges (challenge_id, title, squirrel_id, num_of_total_turns, word_limit_per_turn, genre) values (6, 'Feedfish', 3, 4, 300, 'crime');
+insert into Challenges (challenge_id, title, squirrel_id, num_of_total_turns, word_limit_per_turn, genre) values (7, 'Nlounge', 2, 4, 300, 'fantasy');
+insert into Challenges (challenge_id, squirrel_id, num_of_total_turns, word_limit_per_turn, genre) values (8, 2, 6, 500, 'crime');
+insert into Challenges (challenge_id, title, squirrel_id, num_of_total_turns, word_limit_per_turn, genre) values (9, 'Jaxbean', 10, 6, 500, 'fantasy');
+insert into Challenges (challenge_id, title, squirrel_id, num_of_total_turns, word_limit_per_turn, genre) values (10, 'Kwimbee', 3, 4, 500, 'fantasy');
+insert into Challenges (title, squirrel_id, racoon_id, num_of_total_turns, word_limit_per_turn, genre) values ( 'Kwimbee', 3, 5, 4, 500, 'fantasy');
+
+
+insert into TurnDetails (challenge_id) values (1);
+insert into TurnDetails (challenge_id) values (2);
+insert into TurnDetails (challenge_id) values (3);
