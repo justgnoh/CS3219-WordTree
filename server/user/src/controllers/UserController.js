2267fbe72d8410cbcf2a7e83ad9960595ee8d610
tanboonji <tanboonji@hotmail.com>
2021-10-25 11:15:03 +0800

    Update user service for community service

--- server/user/src/controllers/UserController.js
+++ server/user/src/controllers/UserController.js
@@ -4,6 +4,7 @@ const ERROR_NO_DATA = "Bad Request. No data found.";
 const ERROR_NO_USER_ID = "Bad Request. No user id found.";
 const ERROR_NO_NAME = "Bad Request. No name found.";
 const ERROR_NO_INTEREST = "Bad Request. No interest found.";
+const ERROR_NO_TOTAL_NUT = "Bad Request. No total nut found.";
 
 export async function createUserProfile(req, res) {
     console.log("createUserProfile: ", req.body);
@@ -32,6 +33,78 @@ export async function getUserProfile(req, res) {
         .catch(err => { res.status(500).send(err.message); });
 }
 
+export async function updateUserName(req, res) {
+    console.log("updateUserName: ", req.body);
+    const body = req.body;
+    if (body == undefined) {
+        return res.status(400).send(ERROR_NO_DATA);
+    }
+    if (!body.userId) {
+        return res.status(400).send(ERROR_NO_USER_ID);
+    }
+    if (!body.name) {
+        return res.status(400).send(ERROR_NO_NAME);
+    }
+
+    await userDao.updateUserName(body.userId, body.name)
+        .then(result => { res.status(200).send("OK"); })
+        .catch(err => { res.status(500).send(err.message); });
+}
+
+export async function updateUserTotalNut(req, res) {
+    console.log("updateUserTotalNut: ", req.body);
+    const body = req.body;
+    if (body == undefined) {
+        return res.status(400).send(ERROR_NO_DATA);
+    }
+    if (!body.userId) {
+        return res.status(400).send(ERROR_NO_USER_ID);
+    }
+    if (!body.totalNut) {
+        return res.status(400).send(ERROR_NO_TOTAL_NUT);
+    }
+
+    await userDao.updateUserTotalNut(body.userId, body.totalNut)
+        .then(result => { res.status(200).send("OK"); })
+        .catch(err => { res.status(500).send(err.message); });
+}
+
+export async function addUserInterest(req, res) {
+    console.log("addUserInterest: ", req.body);
+    const body = req.body;
+    if (body == undefined) {
+        return res.status(400).send(ERROR_NO_DATA);
+    }
+    if (!body.userId) {
+        return res.status(400).send(ERROR_NO_USER_ID);
+    }
+    if (!body.interest) {
+        return res.status(400).send(ERROR_NO_INTEREST);
+    }
+
+    await userDao.addUserInterest(body.userId, body.interest)
+        .then(result => { res.status(200).send("OK"); })
+        .catch(err => { res.status(500).send(err.message); });
+}
+
+export async function deleteUserInterest(req, res) {
+    console.log("deleteUserInterest: ", req.body);
+    const body = req.body;
+    if (body == undefined) {
+        return res.status(400).send(ERROR_NO_DATA);
+    }
+    if (!body.userId) {
+        return res.status(400).send(ERROR_NO_USER_ID);
+    }
+    if (!body.interest) {
+        return res.status(400).send(ERROR_NO_INTEREST);
+    }
+
+    await userDao.deleteUserInterest(body.userId, body.interest)
+        .then(result => { res.status(200).send("OK"); })
+        .catch(err => { res.status(500).send(err.message); });
+}
+
 export async function getUserInterest(req, res) {
     console.log("getUserInterest: ", req.params);
     const userId = req.params.userId;
@@ -41,38 +114,11 @@ export async function getUserInterest(req, res) {
         .catch(err => { res.status(500).send(err.message); });
 }
 
-export async function deleteUserInterest(req, res) {
-    console.log("deleteUserInterest: ", req.body);
-    const profile = req.body;
-    if (profile == undefined) {
-        return res.status(400).send(ERROR_NO_DATA);
-    }
-    if (!profile.userId) {
-        return res.status(400).send(ERROR_NO_USER_ID);
-    }
-    if (!profile.interest) {
-        return res.status(400).send(ERROR_NO_INTEREST);
-    }
+export async function clearUserInterest(req, res) {
+    console.log("clearUserInterest: ", req.params);
+    const userId = req.params.userId;
 
-    await userDao.deleteUserInterest(profile.userId, profile.interest)
-        .then(result => { res.status(200).send("OK"); })
-        .catch(err => { res.status(500).send(err.message); });
-}
-
-export async function addUserInterest(req, res) {
-    console.log("addUserInterest: ", req.body);
-    const profile = req.body;
-    if (profile == undefined) {
-        return res.status(400).send(ERROR_NO_DATA);
-    }
-    if (!profile.userId) {
-        return res.status(400).send(ERROR_NO_USER_ID);
-    }
-    if (!profile.interest) {
-        return res.status(400).send(ERROR_NO_INTEREST);
-    }
-
-    await userDao.addUserInterest(profile.userId, profile.interest)
+    await userDao.clearUserInterest(userId)
         .then(result => { res.status(200).send("OK"); })
         .catch(err => { res.status(500).send(err.message); });
 }
--- server/user/src/database/UserProfileDao.js
+++ server/user/src/database/UserProfileDao.js
@@ -5,6 +5,7 @@ const userInterestDb = "UserInterest";
 const userIdCol = "user_id";
 const nameCol = "name";
 const interestCol = "interest";
+const totalNutCol = "total_nut";
 
 export async function createUserProfile(userId, name) {
     try {
@@ -26,10 +27,30 @@ export async function getUserProfile(userId) {
     }
 }
 
-export async function getUserInterest(userId) {
+export async function updateUserName(userId, name) {
     try {
-        const result = await pool.query("SELECT * FROM " + userInterestDb +
-                " WHERE  " + userIdCol + " = $1;", [userId]);
+        const result = await pool.query("UPDATE " + userProfileDb +
+                " SET " + nameCol + " = $1 WHERE " + userIdCol + " = $2;", [name, userId]);
+        return result;
+    } catch (err) {
+        throw err;
+    }
+}
+
+export async function updateUserTotalNut(userId, totalNut) {
+    try {
+        const result = await pool.query("UPDATE " + userProfileDb +
+                " SET " + totalNutCol + " = $1 WHERE " + userIdCol + " = $2;", [totalNut, userId]);
+        return result;
+    } catch (err) {
+        throw err;
+    }
+}
+
+export async function addUserInterest(userId, interest) {
+    try {
+        const result = await pool.query("INSERT INTO " + userInterestDb +
+                "(" + userIdCol + ", " + interestCol + ") VALUES ($1, $2);", [userId, interest]);
         return result;
     } catch (err) {
         throw err;
@@ -46,10 +67,20 @@ export async function deleteUserInterest(userId, interest) {
     }
 }
 
-export async function addUserInterest(userId, interest) {
+export async function getUserInterest(userId) {
     try {
-        const result = await pool.query("INSERT INTO " + userInterestDb +
-                "(" + userIdCol + ", " + interestCol + ") VALUES ($1, $2);", [userId, interest]);
+        const result = await pool.query("SELECT * FROM " + userInterestDb +
+                " WHERE  " + userIdCol + " = $1;", [userId]);
+        return result;
+    } catch (err) {
+        throw err;
+    }
+}
+
+export async function clearUserInterest(userId) {
+    try {
+        const result = await pool.query("DELETE FROM " + userInterestDb +
+                " WHERE  " + userIdCol + " = $1;", [userId]);
         return result;
     } catch (err) {
         throw err;
