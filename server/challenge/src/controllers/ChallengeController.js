07231065bf33a8ec657163d1b35230c95c6a3ef3
Nikhilalalalala <uvnikhi@gmail.com>
2021-11-01 23:00:00 +0800

    Add more error handling and follow DAO pattern

--- server/challenge/src/controllers/ChallengeController.js
+++ server/challenge/src/controllers/ChallengeController.js
@@ -1,117 +1,143 @@
-import express from 'express';
-import pool from '../database/db.js';
-import {SQL_QUERIES as query} from '../database/sql_queries.js';
-import { error_messages, OK_MESSAGE} from '../config/index.js';
+import express from "express";
+import { error_messages, OK_MESSAGE } from "../config/index.js";
+import {
+  getPlayersInChallenge,
+  addChallenge,
+  getNumOfCompletedTurnsForChallenge,
+  updateTurnDetails,
+  getChallengeByUserIdFromDB,
+  getChallengeByChallengeIdFromDB,
+  insertNewTurnDetails,
+} from "../database/ChallengesDao.js";
 
 export const getAllChallengeByUserId = async (req, res) => {
-    const userID = req.query.userid;
-    if (req.query.userid === undefined) {
-        return res.status(400).send(error_messages.MISSING_FIELDS)
-    }
-    const challenge = await pool.query(query.GET_CHALLENGES_BY_USER_ID, [userID]);
-    return res.status(200).json(challenge.rows);
-}
+  const userID = req.query.userid;
+  if (req.query.userid === undefined) {
+    return res.status(400).send(error_messages.MISSING_FIELDS);
+  }
+  const challenges = await getChallengeByUserIdFromDB(userID);
+  return res.status(200).json(challenges);
+};
 
 export const createNewChallenge = async (req, res) => {
-    const data = req.body;
-    if (data === undefined || !data.squirrel_id || !data.num_of_total_turns || !data.word_limit_per_turn || !data.genre) 
-        return res.status(400).send(error_messages.MISSING_FIELDS);
-    if (data.num_of_total_turns !== 4 && data.num_of_total_turns !== 6  ) return res.status(400).send(error_messages.INVALID_FIELDS);
-    if (data.word_limit_per_turn !== 300 && data.word_limit_per_turn !== 500  ) return res.status(400).send(error_messages.INVALID_FIELDS);
+  const userID = req.headers["x-access-token"];
+  const data = req.body;
+  if (
+    data === undefined ||
+    userID === undefined ||
+    !data.num_of_total_turns ||
+    !data.word_limit_per_turn ||
+    !data.interest
+  )
+    return res.status(400).send(error_messages.MISSING_FIELDS);
+  if (data.num_of_total_turns !== 4 && data.num_of_total_turns !== 6)
+    return res.status(400).send(error_messages.INVALID_FIELDS);
+  if (data.word_limit_per_turn !== 300 && data.word_limit_per_turn !== 500)
+    return res.status(400).send(error_messages.INVALID_FIELDS);
 
-    const challenge = await pool.query(query.INSERT_NEW_CHALLENGE, [data.squirrel_id, data.num_of_total_turns, data.word_limit_per_turn, data.genre]).catch(err => {
-        if (err.constraint !== undefined) {
-            if (err.constraint.includes("fkey") > 0) {
-                return res.status(400).send(error_messages.INVALID_FIELDS);
-            }
-        } 
-        return res.status(500).send(err.message);
-    })
-
-    if (challenge && challenge.rows && challenge.rows.length > 0 && challenge.rows[0].challenge_id) {
-            console.log(challenge.rows[0].challenge_id);
-            return await pool.query(query.INSERT_NEW_TURN_DETAILS, [challenge.rows[0].challenge_id])
-            // TODO get words from word service
-            .then(() => res.status(200).send("OK"))
-            .catch(err => {
-                console.log(err);
-                return res.status(500).send(error_messages.INTERNAL_ERROR)
-            });
+  const challenge = await addChallenge(
+    userID,
+    data.num_of_total_turns,
+    data.word_limit_per_turn,
+    data.interest
+  ).catch((err) => {
+    if (err.constraint !== undefined) {
+      if (err.constraint.includes("fkey") > 0) {
+        return res.status(400).send(error_messages.INVALID_FIELDS);
+      }
     }
-}
+    return res.status(500).send(err.message);
+  });
+
+  if (challenge && challenge.length > 0 && challenge[0].challenge_id) {
+    await insertNewTurnDetails(challenge[0].challenge_id)
+      // TODO get words from word service
+      .then(() => res.status(200).send("OK"))
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).send(error_messages.INTERNAL_ERROR);
+      });
+  }
+};
 
 export const addEssayPara = async (req, res) => {
-    const { id } = req.params;
-    const data = req.body;
-    if (!data.player_id) return res.status(400).send(error_messages.MISSING_FIELDS);
-    if (!data.essay_para) return res.status(400).send(error_messages.MISSING_FIELDS);
-    if (isNaN(id)) return res.status(400).send(error_messages.INVALID_FIELDS);
+  let userID = req.headers["x-access-token"];
+  const { id } = req.params;
+  const data = req.body;
 
-    const allPlayerIDs = await pool.query(query.GET_PLAYER_ID_IN_CHALLENGE, [id]).then(
-        (playerIDs) => {
-            
-            if (playerIDs.rows.length === 0 || playerIDs.rows.length > 1 || !playerIDs.rows[0].row) return [];
-            return playerIDs.rows[0].row.match(/\d+/g);
-        }
-    ).catch(err => {
-        return res.status(500).send(err.message);
-    })
+  if (!userID || !data.essay_para)
+    return res.status(400).send(error_messages.MISSING_FIELDS);
+  if (isNaN(id)) return res.status(400).send(error_messages.INVALID_FIELDS);
 
-    if (allPlayerIDs === undefined || allPlayerIDs.length === 0) {
-        return res.status(404).send(error_messages.NO_SUCH_CHALLENGE_FOUND);
-    }
+  userID = parseInt(userID);
 
-    const isCorrectPlayerTurn = await pool.query(query.GET_NUM_OF_COMPLETED_TURNS_FOR_CHALLENGE, [id]).then(
-        numOfCompletedTurnsRes => {
-            if (numOfCompletedTurnsRes.rows && numOfCompletedTurnsRes.rows.length > 0) {
-                // Extracting Results
-                if (numOfCompletedTurnsRes.rows[0].num_of_sequences_completed !== undefined &&
-                    numOfCompletedTurnsRes.rows[0].num_of_sequences_completed % 2 === 0) {
-                    //squirrel's turn
-                    return allPlayerIDs.indexOf(data.player_id) === 0;
-                } 
-            } 
+  const allPlayerIDs = await getPlayersInChallenge(id).catch((err) =>
+    res.status(500).send(err.message)
+  );
+
+  const challenge = await getChallengeByChallengeIdFromDB(id);
+
+  if (allPlayerIDs === undefined || allPlayerIDs.length === 0) {
+    return res.status(404).send(error_messages.NO_SUCH_CHALLENGE_FOUND);
+  }
+
+  const isCorrectPlayerTurn = await getNumOfCompletedTurnsForChallenge(id)
+    .then((numOfCompletedTurnsRes) => {
+      console.log(numOfCompletedTurnsRes);
+      if (numOfCompletedTurnsRes.length > 0) {
+        // Extracting Results
+        if (
+          numOfCompletedTurnsRes[0].num_of_sequences_completed !== undefined
+        ) {
+          let sequenceNum =
+            numOfCompletedTurnsRes[0].num_of_sequences_completed;
+          if (sequenceNum >= numchallenge["num_of_total_turns"]) {
             return false;
+          }
+          return sequenceNum % 2 === 0
+            ? allPlayerIDs.indexOf(userID) === 0 // squirrel turn
+            : allPlayerIDs.indexOf(userID) === 1; // racoon turn
         }
-    ).catch(err => {
-        return res.status(500).send(err.message);
+      }
+      return false;
     })
+    .catch((err) => false);
 
-    // TODO add essay para from essay service
+  // TODO add essay para from essay service
 
-    if (isCorrectPlayerTurn) {
-        await pool.query(query.UPDATE_TURN_DETAILS_TIMESTAMP, [id]).catch(err => {
-            return res.status(500).send(err.message);
-        })
-        await pool.query(query.UPDATE_TURN_DETAILS_SEQUENCE_NUM, [id]).catch(err => {
-            return res.status(500).send(err.message);
-        })
-        return res.status(200).send(OK_MESSAGE)
-    } else {
-        return res.status(403).send(error_messages.WRONG_TURN);
-    }
-
-}
+  if (isCorrectPlayerTurn) {
+    await updateTurnDetails(id).catch((err) => {
+      return res.status(500).send(err.message);
+    });
+    return res.status(200).send(OK_MESSAGE);
+  } else {
+    return res.status(403).send(error_messages.WRONG_TURN);
+  }
+};
 
 export const getChallengeByChallengeId = async (req, res) => {
-    const userID = req.query.userid;
-    const { id } = req.params;
-
-    if (!userID) {
-        return res.status(400).send(error_messages.MISSING_FIELDS)
+  const userID = req.headers["x-access-token"];
+  if (!userID) {
+    return res.status(400).send(error_messages.MISSING_FIELDS);
+  }
+  const params = req.params;
+  if (!params) {
+    return res.status(400).send(error_messages.MISSING_FIELDS);
+  }
+  const id = params["id"];
+  const challenges = await getChallengeByChallengeIdFromDB(id);
+  if (challenges.length > 1) {
+    console.log("Duplicate Challenge ID found");
+    return res.status(500).send(error_messages.INTERNAL_ERROR);
+  }
+  if (challenges[0]["status_of_challenge"] != "COMPLETED") {
+    if (
+      userID != challenges[0]["squirrel_id"] &&
+      userID != challenges[0]["racoon_id"]
+    ) {
+      return res.status(401).send(error_messages.NOT_IN_THIS_CHALLENGE);
     }
-
-    const challenge = await pool.query(query.GET_CHALLENGE_BY_CHALLENGE_ID, [id]);
-    if (challenge.rows.length > 1) {
-        console.log("Duplicate Challenge ID found");
-        return res.status(500).send(error_messages.INTERNAL_ERROR);
-    }
-    if (challenge.rows[0]['status_of_challenge'] != "COMPLETED" ) {
-        if (userID != challenge.rows[0]['squirrel_id'] && userID != challenge.rows[0]['racoon_id']) {
-            return res.status(401).send(error_messages.NOT_IN_THIS_CHALLENGE)
-        } 
-    }
-
-    // TODO Talk to essay service and get all essay para
-    return res.status(200).json(challenge.rows[0]);
-}
+  }
+  // TODO Talk to essay service and get all essay para
+  return res.status(200).json(challenges[0]);
+};
---
+++ server/challenge/src/database/ChallengesDao.js
@@ -0,0 +1,74 @@
+import pool from '../database/db.js';
+
+const challengeDb = "Challenges";
+
+export async function addChallenge(squirrel_id, num_of_total_turns, word_limit_per_turn, interest) {
+    try {
+        const result = await pool.query("INSERT into " + challengeDb + "(squirrel_id, num_of_total_turns, word_limit_per_turn, interest) values ($1, $2, $3, $4) RETURNING challenge_id", [
+            squirrel_id, num_of_total_turns, word_limit_per_turn, interest
+        ]);
+        return result.rows;
+    } catch (err) {
+        throw err;
+    }
+}
+
+export async function getPlayersInChallenge(challengeID) {
+    try {
+        const result = await pool.query("SELECT (squirrel_id, racoon_id) FROM Challenges c WHERE c.challenge_id = $1", [challengeID]).then(
+            (playerIDs) => {
+                if (playerIDs.rows.length === 0 || playerIDs.rows.length > 1 || !playerIDs.rows[0].row) return [];
+                const playerIDString = playerIDs.rows[0].row.match(/\d+/g);
+                return playerIDString.map(str => parseInt(str));
+            }
+        )
+        return result;
+    } catch (err) {
+        throw err;
+    }
+}
+
+export async function getNumOfCompletedTurnsForChallenge(challengeID) {
+    try {
+        const result = await pool.query("SELECT num_of_sequences_completed FROM TurnDetails t WHERE t.challenge_id = $1", [challengeID]);
+        return result.rows;
+    } catch (err) {
+        throw err;
+    }
+}
+
+export async function getChallengeByChallengeIdFromDB(challengeID) {
+    try {
+        const result = await pool.query("SELECT * FROM Challenges c WHERE c.challenge_id = $1", [challengeID]);
+        return result.rows;
+    } catch (err) {
+        throw err;
+    }
+}
+
+export async function getChallengeByUserIdFromDB(userID) {
+    try {
+        const result = await pool.query("SELECT * FROM Challenges WHERE squirrel_iD = $1 OR racoon_id = $1", [userID])
+        return result.rows;
+    } catch (err) {
+        throw err;
+    }
+}
+
+export async function updateTurnDetails(challengeID) {
+    try {
+        await pool.query("UPDATE TurnDetails SET time_of_last_completed_sequence = CURRENT_TIMESTAMP  WHERE challenge_id = $1", [challengeID]);
+        await pool.query("UPDATE TurnDetails SET num_of_sequences_completed = num_of_sequences_completed + 1 WHERE challenge_id = $1", [challengeID]);
+    } catch (err) {
+        throw err;
+    }
+}
+
+export async function insertNewTurnDetails(challengeID) {
+    try {
+        //Creates a row in the turn table but does not start a turn
+        await pool.query("insert into TurnDetails(challenge_id) values ($1)", [challengeID]);
+    } catch (err) {
+        throw err;
+    }
+}
