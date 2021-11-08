ecd3a55dd80fbbacb0574c3db2a0c39c04bfaff7
tanboonji <tanboonji@hotmail.com>
2021-11-08 19:03:36 +0800

    Fix endpoints regarding user interest

--- server/user/src/routes/UserRouter.js
+++ server/user/src/routes/UserRouter.js
@@ -8,10 +8,7 @@ router.get('/getUserProfile', userController.getUserProfile);
 router.put('/updateUserProfile', userController.updateUserProfile);
 router.put('/updateUserTotalNut', userController.updateUserTotalNut);
 router.get('/getInterest', userController.getInterest);
-router.post('/addUserInterest', userController.addUserInterest);
-router.delete('/deleteUserInterest', userController.deleteUserInterest);
 router.get('/getUserInterest', userController.getUserInterest);
-router.delete('/clearUserInterest', userController.clearUserInterest);
 router.put('/updateUserInterest', userController.updateUserInterest);
 
 export default router;
